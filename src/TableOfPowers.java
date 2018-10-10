c67105685247d4efb0c184b583959f7271ae237f
benjamin <benjamin.mcbrayer@gmail.com>
2018-10-10 14:08:33 -0400

    October revisions

--- src/TableOfPowers.java
+++ src/TableOfPowers.java
@@ -8,17 +8,18 @@ public class TableOfPowers {
 
 	public static void main(String[] args) {
 
+		// Declare and initialize variables.
 		Scanner scnr = new Scanner(System.in);
 		int userNum = 0;
-		int i;
 		String playAgain = null;
-		String userInfo;
+		String userInfo = null;
 
+		// Introduce the program.
 		System.out.println("Learn your squares and cubes!");
 
 		// Ask for user information.
 		System.out.print("Please enter your name: ");
-		userInfo = scnr.next();
+		userInfo = scnr.nextLine();
 		System.out.println("Hello, " + userInfo + "!");
 
 		do {// Prompt user to enter an integer.
@@ -26,13 +27,10 @@ public class TableOfPowers {
 			userNum = scnr.nextInt();
 
 			// Create table.
-			System.out.printf("%1$-10s %2$-10s %3$-10s", "Number", "Squared", "Cubed");
-			System.out.printf("\n%1$-10s %2$-10s %3$-10s", "=======", "=======", "======");
+			displayTable();
 
 			// Calculate and print numbers, squares, and cubes from 1 to the value entered.
-			for (i = 1; i <= userNum; ++i) {
-				System.out.printf("\n%1$-10s %2$-10s %3$-10s", i, (long) Math.pow(i, 2), (long) Math.pow(i, 3));
-			}
+			calculatePowers(userNum);
 
 			// Ask if the user wants to continue entering numbers.
 			System.out.println();
@@ -45,9 +43,19 @@ public class TableOfPowers {
 		// End program officially if user enters anything other than "Y" or "y."
 		System.out.println("Goodbye, " + userInfo + "!");
 
-
 		scnr.close();
 
+	}
+
+	public static void calculatePowers(int userNum) {
+		for (int i = 1; i <= userNum; ++i) {
+			System.out.printf("\n%1$-10s %2$-10s %3$-10s", i, (long) Math.pow(i, 2), (long) Math.pow(i, 3));
+		}
+	}
+
+	public static void displayTable() {
+		System.out.printf("%1$-10s %2$-10s %3$-10s", "Number", "Squared", "Cubed");
+		System.out.printf("\n%1$-10s %2$-10s %3$-10s", "=======", "=======", "======");
 	}
 
 }
